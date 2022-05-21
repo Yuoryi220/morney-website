4f80e7982f5c2d2e5c54a63449cc6866a2e96e63
diaoyue <1264225436@qq.com>
2022-05-21 11:15:43 +0800

    deploy

---
+++ precache-manifest.b1a1f4d60f494ba8a9a95103460239b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4305aa3eafb587b16a54",
+    "url": "/morney-website/css/app.29a47ba0.css"
+  },
+  {
+    "revision": "af2ab39296e9665ac8dfffd586d846af",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "4305aa3eafb587b16a54",
+    "url": "/morney-website/js/app.c51439a0.js"
+  },
+  {
+    "revision": "b8a63fa27e8826f3ac88",
+    "url": "/morney-website/js/chunk-vendors.49abd244.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
